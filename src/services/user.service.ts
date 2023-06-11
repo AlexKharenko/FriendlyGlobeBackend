a13dc9ca5f42c683501a9de58b7ab4a7a389dd2b
AlexKharenko <alexey16052002@gmail.com>
2023-06-11 21:21:57 +0300

    Changed page size from 10 to 12 users

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -22,7 +22,7 @@ import { pipeline } from 'stream';
 import { createFoldersIfNotExist } from 'src/utils/createFoldersIfNotExist';
 const pump = util.promisify(pipeline);
 
-const PAGE_SIZE = 10;
+const PAGE_SIZE = 12;
 
 @Injectable()
 export class UserService {
